464f850dd4fb3828a687885a365c33f294227f0d
maomarcus <marcus_mao@yahoo.com>
2017-09-17 01:40:33 -0500

    gome over changer

--- src/trash/states/GameOver.java
+++ src/trash/states/GameOver.java
@@ -4,6 +4,7 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
+import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -12,10 +13,21 @@ import trash.Application;
 
 public class GameOver extends BasicGameState {
     private boolean shouldToggleFullscreen;
+    private Music gameOver;
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-        // TODO Auto-generated method stub
+        gameOver = new Music("res/gameover.ogg");
+    }
+    
+    @Override
+    public void enter(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        gameOver.play();
+    }
+    
+    @Override
+    public void leave(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        gameOver.stop();
     }
 
     @Override

8c16560098a4cf818891b9f9781d329c4c28f953
James Dong <dong.james.sept14@gmail.com>
2017-09-17 12:52:58 -0500

    Runner

---
+++ run.bat
@@ -0,0 +1,1 @@
+java -cp bin;libs/slick2d/lib/jinput.jar;libs/slick2d/lib/jogg-0.0.7.jar;libs/slick2d/lib/jorbis-0.0.15.jar;libs/slick2d/lib/slick.jar;libs/slick2d/lib/lwjgl.jar;libs/json-simple-1.1.1.jar;libs/slick2d/lib/lwjgl_util.jar  trash.Application -Djava.library.path=libs/lwjgl-2.9.3/native/windows

3bd862e239393d77cc4a05bac796ffb7b668d9bd
maomarcus <marcus_mao@yahoo.com>
2017-09-17 10:45:24 -0500

    intersect margin location change

--- src/trash/objects/GroundGoon.java
+++ src/trash/objects/GroundGoon.java
@@ -2,8 +2,6 @@ package trash.objects;
 
 public abstract class GroundGoon extends Goon{
 	
-    public static final int INTERSECT_MARGIN = 10;
-	
 	public GroundGoon(Player play)
     {
 		super(play);
